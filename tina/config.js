a3665686443f1f38b93b1958155fdd75ab2122cc
Jaycee Lydian <scragz@users.noreply.github.com>
2024-10-09 12:20:03 -0700

    11ty tina

---
+++ tina/config.js
@@ -0,0 +1,81 @@
+import { defineConfig as defineConfig } from "tinacms";
+
+const branch = process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+const isLocal = process.env.TINA_PUBLIC_IS_LOCAL === "true";
+
+const config = {
+  token: process.env.TINA_TOKEN,
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  branch,
+  isLocal,
+  build: {
+    outputFolder: "admin",
+    publicFolder: "_site",
+  },
+  media: {
+    tina: {
+      mediaRoot: "assets/images",
+      publicFolder: "_site",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "pages",
+        label: "Pages",
+        path: "src",
+        match: {
+          include: "{*,resources/*}",
+        },
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/_posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+  search: {
+    tina: {
+      indexerToken: '31fd0124b14f48f240882c8c915114c007c86268',
+      stopwordLanguages: ['eng'],
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100,
+  },
+};
+export default defineConfig(config);
